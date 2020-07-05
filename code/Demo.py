d2807328c90a5d31fcb2c484a9c745d33b79085c
My Computer <thieunv.nb@example.com>
2020-07-06 00:37:02 +0700

    done bao cao va code rate 83.3%

--- code/Demo.py
+++ code/Demo.py
@@ -63,7 +63,7 @@ class HOG:
         return vote
 
     @staticmethod
-    def hog_feature(img, cells=(8, 8), blocks=(8, 8),  bin_num=9):
+    def hog_feature(img, cells=(8, 8), blocks =(1,1),  bin_num=9):
         magnitude_arr, direction_arr = HOG.calculate_gradients(img)
         max_w = magnitude_arr.shape[1] // cells[0]
         max_h = magnitude_arr.shape[0] // cells[1]
@@ -77,7 +77,7 @@ class HOG:
                 cells_array.append(val)
         cells_array = np.asarray(cells_array)
         # tinh toan feature theo norm trung binh
-        cells_array = np.reshape(cells_array, blocks[0], blocks[1], cells_array.shape[1])
+        cells_array = np.reshape(cells_array, cells_array.shape[0] * cells_array.shape[1])
         return cells_array
 
 
@@ -99,25 +99,93 @@ class KNN:
         # tinh toan khoang cach den cac diem
         for i in range(len(x)):
             d = KNN.caculate_distance(x[i], f_predict)
-            array_distance.append[d]
+            array_distance.append(d)
+        array_distance_sorted = sorted(array_distance, key = lambda x : x, reverse=False)
+        label_nn = []
+        for i in range(k):
+            for _i in range(len(array_distance)):
+                if(array_distance_sorted[i] == array_distance[_i]):
+                    label_nn.append(y[_i])
+        print(label_nn)
+        label_predict = 0
+        count_max = 0
+        for lb in label_nn:
+            count = label_nn.count(lb)
+            if(count > count_max):
+                count_max = count
+                label_predict = lb
+        return label_predict
 
-        array_distance_sorted = sorted()
 
-
-
-def main():
-    img = cv2.imread("1.jpg", 0)
-    img2 = cv2.imread("tesst.png", 0)
-    feature = HOG.hog_feature(img)
-    # feature2 = HOG.hog_feature(img2)
-    # KNN.predict([feature], [0], 1, feature2)
-    # print(feature.shape)
 
 
 def Tshirt_class():
     # load toan bo anh o thu muc so sanh cho ao
-    files = glob.glob()
+    ao_coc = glob.glob(r"anh so sanh\ao\ao coc\*.jpg")
+    so_mi = glob.glob(r"anh so sanh\ao\so mi\*.jpg")
+    vay = glob.glob(r"anh so sanh\ao\vay\*.jpg")
+    # ao coc label = 0, so mi = 1, vay = 2
+    x = []
+    y = []
+    print("[INFO] Load data  de so sanh...")
+    for f in ao_coc:
+        img = cv2.imread(f, 0)
+        img = cv2.resize(img, (128,128))
+        feature = HOG.hog_feature(img)
+        x.append(feature)
+        y.append(0)
+    for f in so_mi:
+        img = cv2.imread(f, 0)
+        img = cv2.resize(img, (128,128))
+        feature = HOG.hog_feature(img)
+        x.append(feature)
+        y.append(1)
+    for f in vay:
+        img = cv2.imread(f, 0)
+        img = cv2.resize(img, (128,128))
+        feature = HOG.hog_feature(img)
+        x.append(feature)
+        y.append(2)
+    print("[INFO] Load data  can du doan...")
+    img_predict = cv2.imread(r"dudoan\ao\6.jpg", 0)
+    f_predict = HOG.hog_feature(img_predict)
+    classes = KNN.predict(x, y, 3, f_predict)
+    if(classes == 0):
+        print("[INFO] DAY LA AO COC!")
+    elif(classes == 1):
+        print("[INFO] DAY LA AO SO MI!")
+    elif(classes == 2):
+        print("[INFO] DAY LA AO VAY!")
+
+def Short_Class():
+    quan_dui = glob.glob(r"anh so sanh\quan\quan dui\*.jpg")
+    quan_dai = glob.glob(r"anh so sanh\quan\quan dai\*.jpg")
+    # ao coc label = 0, so mi = 1, vay = 2
+    x = []
+    y = []
+    print("[INFO] Load data  de so sanh...")
+    for f in quan_dui:
+        img = cv2.imread(f, 0)
+        img = cv2.resize(img, (128,128))
+        feature = HOG.hog_feature(img)
+        x.append(feature)
+        y.append(0)
+    for f in quan_dai:
+        img = cv2.imread(f, 0)
+        img = cv2.resize(img, (128,128))
+        feature = HOG.hog_feature(img)
+        x.append(feature)
+        y.append(1)
+    print("[INFO] Load data  can du doan...")
+    img_predict = cv2.imread(r"dudoan\quan\4.jpg", 0)
+    img_predict = cv2.resize(img_predict, (128,128))
+    f_predict = HOG.hog_feature(img_predict)
+    classes = KNN.predict(x, y, 9, f_predict)
+    if(classes == 0):
+        print("[INFO] DAY LA QUAN DUI!")
+    elif(classes == 1):
+        print("[INFO] DAY LA QUAN DAI!")
 
 
 if __name__ == "__main__":
-    main()
+    Short_Class()
